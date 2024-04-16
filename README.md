687305220bc167b39a9a90a668b1b8f0225957fd
T4910 <taiwo.emmanuel@lmu.edu.ng>
2024-04-16 15:18:56 +0100

    [!UPDATE] editted env variable names & Readme

--- README.md
+++ README.md
@@ -12,5 +12,14 @@
  -Clone app using this [link](http://github.com/T4910/OctaEngine)
  -Install the dependencies using `npm install`
  -Create a `.env` file `touch .env`
- -In the env file, set your variables: `PORT=8080` and `WEBSITE=http://localhost:5173` and `MONGO_URI=yourMongoUri`
- -Run the app using `node --watch server`
+ -In the env file, set your variables: 
+ 1. `PORT`
+Set it to `8080` or any port you would prefer
+
+2. `WEBISTE`
+Front end is running on `http://localhost:5173`.
+
+3. `MONGO_URL`
+Just the name of your mongo database url
+
+-Run the app using `node --watch server`
--- server.js
+++ server.js
@@ -35,20 +35,144 @@ app.get('/', async (req, res) => {
     return res.status(200).json({ message: "Welcome to team Octagon." })
 })
 
+// app.use('/timetable', timetableRoute)
+app.get('/schedule', async (req, res) => {
+  const { level, department } = req.body;
+
+  // dummy data to be gotten from database
+    const selectedSchedule = {
+        semester: 1,
+        year: '2023/2024',
+        mode: 'classes', // or Exam
+    
+        startDay: 'Monday',
+        endDay: 'Saturday',
+        lectureHours: {
+            startHour: '8:00 AM',
+            endHour: '6:00 PM'
+        },
+        departments: {
+            'Computer Science': {
+                100: {
+                    monday: [
+                        {
+                          id: 1,
+                          name: "TMC 222",
+                          venue: 'LT1',
+                          event: {
+                            type: "lecture",
+                            coordinator: 'Mr. Shaba'
+                          },
+                          startTime: new Date("2018-02-23T15:00:00"),
+                          endTime: new Date("2018-02-23T16:00:00"),
+                        },
+                        {
+                          id: 1,
+                          name: "CSC 222",
+                          venue: 'LT1',
+                          event: {
+                            type: "lecture",
+                            coordinator: 'Mr. Shaba'
+                          },
+                          startTime: new Date("2018-02-23T11:00:00"),
+                          endTime: new Date("2018-02-23T12:00:00"),
+                        },
+                      ],
+                    tuesday: [],
+                    wednesday : [
+                        {
+                          id: 1,
+                          name: "CSC 212",
+                          venue: 'LT1',
+                          event: {
+                            type: "lecture",
+                            coordinator: 'Mr. Shaba'
+                          },
+                          startTime: new Date("2018-02-23T13:00:00"),
+                          endTime: new Date("2018-02-23T15:00:00"),
+                        },
+                        {
+                          id: 2,
+                          name: "CSC 217",
+                          venue: 'LT1',
+                          event: {
+                            type: "lecture",
+                            coordinator: 'Mr. Shaba'
+                          },
+                          startTime: new Date("2018-02-23T15:00:00"),
+                          endTime: new Date("2018-02-23T16:00:00"),
+                        },
+                      ],
+                    thursday: [],
+                    friday: [],
+                    saturday: [],
+                },
+                200: {
+                    monday: [
+                        {
+                          id: 1,
+                          name: "CSC 222",
+                          event: {
+                            type: "lecture",
+                            venue: 'LT1',
+                            coordinator: 'Mr. Shaba'
+                          },
+                          startTime: new Date("2018-02-23T11:30:00"),
+                          endTime: new Date("2018-02-23T13:30:00"),
+                        },
+                      ],
+                },
+                300: {
+                    monday: [
+                        {
+                          id: 1,
+                          name: "CSC 222",
+                          event: {
+                            type: "lecture",
+                            venue: 'LT1',
+                            coordinator: 'Mr. Shaba'
+                          },
+                          startTime: new Date("2018-02-23T11:30:00"),
+                          endTime: new Date("2018-02-23T13:30:00"),
+                        },
+                      ],
+                },
+                400: {
+                    monday: [
+                        {
+                          id: 1,
+                          name: "CSC 222",
+                          event: {
+                            type: "lecture",
+                            venue: 'LT1',
+                            coordinator: 'Mr. Shaba'
+                          },
+                          startTime: new Date("2018-02-23T11:30:00"),
+                          endTime: new Date("2018-02-23T13:30:00"),
+                        },
+                      ],
+                },
+            }
+        }
+    }
+    
+    return res.status(200).json({schedule: selectedSchedule});
+})
+  
+  
 //! routes
 app.use('/user', userRoute)
 // app.use('/department', departmentRoute)
 // app.use('/course', courseRoute)
-// app.use('/timetable', timetableRoute)
 // app.use('/timeslot', timeslotRoute)
 // app.use('/activity', activityRoute)
 // app.use('/issue', issueRoute)
 // app.use('/report', reportRoute)
 
-const mongo_uri = process.env.MONGO_URI
+const mongo_url = process.env.MONGO_URL
 
 //! connect to mongodb
-mongoose.connect(`${mongo_uri}/landmark`)
+mongoose.connect(`${mongo_url}/landmark`)
     .then(() => {
         //listen
         app.listen(port, console.log(`app is connected to mongodb server and running on port ${port}...`))
