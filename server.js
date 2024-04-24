563a944d867c1d498cd92b443c12529826ef5dd2
T4910 <taiwo.emmanuel@lmu.edu.ng>
2024-04-24 20:03:09 +0100

    for review

--- server.js
+++ server.js
@@ -35,74 +35,74 @@ app.use((req, res, next) => {
 app.get('/', async (req, res) => {
     // Define dummy data
 
-    // const dummyData = [
-    //     {
-    //       current: true,
-    //       name: 'Spring Semester',
-    //       description: 'Spring semester timetable',
-    //       status: 'active',
-    //       semester: 'alpha',
-    //       academicYear: '2023-2024',
-    //       timing: {
-    //         startDay: 'monday',
-    //         endDay: 'friday',
-    //         startHour: new Date('2010-01-01T08:00:00.000Z'),
-    //         endHour: new Date('2010-01-01T18:00:00.000Z'),
-    //         intervals: 60
-    //       },
-    //       type: 'class',
-    //       schedule: [
-    //         {
-    //           level: 100,
-    //           departmentId: '662040c708c11ca848fffa12',
-    //           day: 'monday',
-    //           venueId: '662040c708c11ca848fffa10',
-    //           courseId: '6620421cf9127d181976a91c',
-    //           event: 'lecture',
-    //           coordinator: '661fe4a166812c7a186da26a',
-    //           startTime: new Date('2024-04-19T08:00:00.000Z'),
-    //           endTime: new Date('2024-04-19T10:00:00.000Z')
-    //         },
-    //         // Add more schedule items as needed
-    //       ]
-    //     },
-    //     {
-    //       current: false,
-    //       name: 'Fall Semester',
-    //       description: 'Fall semester timetable',
-    //       status: 'draft',
-    //       semester: 'omega',
-    //       academicYear: '2023-2024',
-    //       timing: {
-    //         startDay: 'monday',
-    //         endDay: 'friday',
-    //         startHour: new Date('2010-01-01T08:00:00.000Z'),
-    //         endHour: new Date('2010-01-01T18:00:00.000Z'),
-    //         intervals: 60
-    //       },
-    //       type: 'class',
-    //       schedule: [
-    //         {
-    //           level: 200,
-    //           departmentId: '662040c708c11ca848fffa12',
-    //           day: 'tuesday',
-    //           venueId: '662040c708c11ca848fffa10',
-    //           courseId: '6620421cf9127d181976a91c',
-    //           event: 'lecture',
-    //           coordinator: '661fe4a166812c7a186da26a',
-    //           startTime: new Date('2024-04-20T10:00:00.000Z'),
-    //           endTime: new Date('2024-04-20T12:00:00.000Z')
-    //         },
-    //         // Add more schedule items as needed
-    //       ]
-    //     },
-    //     // Add more dummy records as needed
-    //   ];
+    const dummyData = [
+        {
+          current: true,
+          name: 'Spring Semester',
+          description: 'Spring semester timetable',
+          status: 'active',
+          semester: 'alpha',
+          academicYear: '2023-2024',
+          timing: {
+            startDay: 'monday',
+            endDay: 'friday',
+            startHour: new Date('2010-01-01T08:00:00.000Z'),
+            endHour: new Date('2010-01-01T18:00:00.000Z'),
+            intervals: 60
+          },
+          type: 'class',
+          schedule: [
+            {
+              level: 100,
+              departmentId: '662040c708c11ca848fffa12',
+              day: 'monday',
+              venueId: '662040c708c11ca848fffa10',
+              courseId: '6620421cf9127d181976a91c',
+              event: 'lecture',
+              coordinator: '661fe4a166812c7a186da26a',
+              startTime: new Date('2024-04-19T08:00:00.000Z'),
+              endTime: new Date('2024-04-19T10:00:00.000Z')
+            },
+            // Add more schedule items as needed
+          ]
+        },
+        {
+          current: false,
+          name: 'Fall Semester',
+          description: 'Fall semester timetable',
+          status: 'draft',
+          semester: 'omega',
+          academicYear: '2023-2024',
+          timing: {
+            startDay: 'monday',
+            endDay: 'friday',
+            startHour: new Date('2010-01-01T08:00:00.000Z'),
+            endHour: new Date('2010-01-01T18:00:00.000Z'),
+            intervals: 60
+          },
+          type: 'class',
+          schedule: [
+            {
+              level: 200,
+              departmentId: '662040c708c11ca848fffa12',
+              day: 'tuesday',
+              venueId: '662040c708c11ca848fffa10',
+              courseId: '6620421cf9127d181976a91c',
+              event: 'lecture',
+              coordinator: '661fe4a166812c7a186da26a',
+              startTime: new Date('2024-04-20T10:00:00.000Z'),
+              endTime: new Date('2024-04-20T12:00:00.000Z')
+            },
+            // Add more schedule items as needed
+          ]
+        },
+        // Add more dummy records as needed
+      ];
 
-    //  const t = await Timetable.insertMany(dummyData);
+     const t = await Timetable.insertMany(dummyData);
     return res.status(200).json({
         message: "Welcome to team Octagon.",
-        // t
+        t
     });
 })
 
